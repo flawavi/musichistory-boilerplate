9da85e13ef9e9b30d26150a40168eca5b0c37543
Fletcher Watson <fbwthe6@gmail.com>
2016-08-04 15:41:25 -0500

    converted xhr and vanilla js to $

--- events.js
+++ events.js
@@ -1,34 +1,33 @@
 // add event listeners to make addView and listView appear and disappear
 
-var listMusic = document.getElementById("list-music");
-var listView = document.getElementById("list-view");
-var addMusic = document.getElementById("add-music");
-var addView = document.getElementById("add-view");
+var listMusic = $("#list-music");
+var listView = $("#list-view");
+var addMusic = $("#add-music");
+var addView = $("#add-view");
 
-addMusic.addEventListener("click", function(){
+addMusic.click( function(){
   console.log("hello add view");
-  addView.classList.remove("hidden");
-  listView.classList.add("hidden");
+  addView.toggleClass("hidden");
+  listView.toggleClass("hidden");
 })
 
-listMusic.addEventListener("click", function(){
+listMusic.click( function(){
   console.log("hello list view")
-  listView.classList.remove("hidden")
-  addView.classList.add("hidden")
+  listView.toggleClass("hidden")
+  addView.toggleClass("hidden")
 })
 
-var button = document.getElementById("btn");
+var button = $("#btn");
 
-
-button.addEventListener("click", function() {
-  var userSong = document.getElementById("userSong").value;
-  var userArtist = document.getElementById("userArtist").value;
-  var userAlbum = document.getElementById("userAlbum").value;
+button.click( function() {
+  var userSong = $("#userSong").val();
+  var userArtist = $("#userArtist").val();
+  var userAlbum = $("#userAlbum").val();
   var newSong = userSong + " by " + userArtist + " on the album " + userAlbum + ".";
-  console.log("hello")
   joinedSongs.push(newSong);
   addToDom(userSong, userArtist, userAlbum);
 })
+
 
 
 
--- songs.js
+++ songs.js
@@ -1,11 +1,15 @@
+
 var songBank = (function () {
   var favoriteSongs = [];
+  var songsJSON;
+  var moreSongsJSON;
 
   return {
-    getFavoriteSongs: function () {
-      var container = document.querySelector("#song-container")
+    getFavoriteSongs: function (songsJSON) {
+      console.log(songsJSON)
+      var container = $("#song-container")
       songsJSON.forEach(function(songs) {
-        container.innerHTML += `
+        container.append(`
     <div id="${songs.name.replace(/\s/g, "")}" class="song">
         <h1>${songs.name}</h1>
           <div class="details">
@@ -13,44 +17,48 @@ var songBank = (function () {
             <h2>${songs.album}</h2>
             <h2>${songs.genre}</h2><button id="deleteButton" class="btn">Say Bye Bye</button>
           </div>
-      </div>`
+      </div>`)
       })
     },
-    loadFavoriteSongs: function (callback) {
-      var xhr = new XMLHttpRequest();
-      xhr.open("GET", "songs.json");
-      xhr.addEventListener("load", function (evt) {
-        songsJSON = JSON.parse(evt.target.responseText).songs;
-        callback(favoriteSongs);
-      });
-      xhr.send();
-    },
-    loadMoreFavoriteSongs: function (callback) {
-      var xhr = new XMLHttpRequest();
-      xhr.open("GET", "more-songs.json");
-      xhr.addEventListener("load", function (evt) {
-        songsJSON = JSON.parse(evt.target.responseText).songs;
-        callback(favoriteSongs);
-      });
-      xhr.send();
-    }
+    // loadFavoriteSongs: function (callback) {
+    //   var xhr = new XMLHttpRequest();
+    //   xhr.open("GET", "songs.json");
+    //   xhr.addEventListener("load", function (evt) {
+    //     songsJSON = JSON.parse(evt.target.responseText).songs;
+    //     callback(favoriteSongs);
+    //   });
+    //   xhr.send();
 
-  };
+      // $.getJSON("songs.json", function (res) {
+      //    songsJSON = res.data
+      // },
+      loadMoreFavoriteSongs: function(cb){
+        $.getJSON("more-songs.json", function (res) {
+        moreSongsJSON = res.songs;
+        cb(moreSongsJSON)
+        })
+      },
+      loadMoreFavoriteSongs: function(cb){
+        $.getJSON("songs.json", function (res) {
+        SongsJSON = res.songs;
+        cb(SongsJSON)
+        })
+      }
+    }
 
 
 })(songBank || {});
 
 
-songBank.loadFavoriteSongs(songBank.getFavoriteSongs)
-document.querySelector("#song-container").addEventListener("click", function(evt){
-
+songBank.loadMoreFavoriteSongs(songBank.getFavoriteSongs)
+$("#song-container").click(function(evt){
   if (evt.target.id === "deleteButton") {
     evt.target.closest('.song').remove()
   }
 })
 
-var moreSongsBtn = document.getElementById("moreSongsBtn");
-moreSongsBtn.addEventListener("click", function () {
+var moreSongsBtn = $("#moreSongsBtn");
+moreSongsBtn.click(function () {
   songBank.loadMoreFavoriteSongs(songBank.getFavoriteSongs)
 })
 
@@ -59,7 +67,7 @@ moreSongsBtn.addEventListener("click", function () {
 
 // var songs = [];
 // var stringSong = [];
-// var joinedSongs = [];
+var joinedSongs = [];
 
 // songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
 // songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
@@ -103,38 +111,36 @@ moreSongsBtn.addEventListener("click", function () {
 //     console.log(songs)
 // }
 
-// var addToDom = function (song, artist, album) {
-//   // for (var i = 0; i < songs.length; i++) {
-//   document.getElementById("song-container").innerHTML += `
-//     <div class="song">
-//         <h1>${song}</h1>
-//           <div class="details">
-//             <h2>${artist}</h2>
-//             <h2>${album}</h2>
-//             <h2>Genre</h2>
-//           </div>
-//       </div>`
+var addToDom = function (song, artist, album) {
+  // for (var i = 0; i < songs.length; i++) {
+  $("#song-container").append(`
+    <div class="song">
+        <h1>${song}</h1>
+          <div class="details">
+            <h2>${artist}</h2>
+            <h2>${album}</h2>
+            <h2>Genre</h2>
+          </div>
+      </div>`)
 
-//       document.getElementById("userSong").value = ""
-//       document.getElementById("userArtist").value = "";
-//       document.getElementById("userAlbum").value = "";
-
-
-//   // }
-// }
-
-// var songDetails = function () {
-//   joinedSongs.forEach(function(test){
-
-//     var songName = test.slice(0, test.indexOf("-")-1);
-//     var artistName = test.slice(test.indexOf("by")+3, test.indexOf("on the album"));
-//     var albumName = test.slice(test.indexOf("album")+6);
-
-//     addToDom(songName, artistName, albumName);
-//   })
-// }
+      $("#userSong").val() = ""
+      $("#userArtist").val() = "";
+      $("#userAlbum").val() = "";
 
 
+  // }
+}
+
+var songDetails = function () {
+  joinedSongs.forEach(function(test){
+
+    var songName = test.slice(0, test.indexOf("-")-1);
+    var artistName = test.slice(test.indexOf("by")+3, test.indexOf("on the album"));
+    var albumName = test.slice(test.indexOf("album")+6);
+
+    addToDom(songName, artistName, albumName);
+  })
+}
 // var fireEvents = function () {
 //   stringify();
 //   replaceChar();
